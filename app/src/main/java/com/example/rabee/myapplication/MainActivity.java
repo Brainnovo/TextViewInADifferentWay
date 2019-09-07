15bbf343652d1e35a7cd2a549fc430a0e15e3584
Brainnovo <rcelias.20@outlook.com>
2019-09-07 08:58:13 +0300

    fixed few things

--- app/src/main/java/com/example/rabee/myapplication/MainActivity.java
+++ app/src/main/java/com/example/rabee/myapplication/MainActivity.java
@@ -21,26 +21,42 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = MainActivity.class.getSimpleName();
-    private RecyclerView rv;
+    private RecyclerView rv_before;
+    private RecyclerView rv_after;
     private Button b;
+    private List<String> one = new ArrayList<>();
+    private List<String> two = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        rv = (RecyclerView) findViewById(R.id.rv);
-        rv.setHasFixedSize(true);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this);
-        rv.setLayoutManager(linearLayoutManager);
-        List<String> fixed = new ArrayList<>();
-        List<String> variable = new ArrayList<>();
-        for (int i = 0; i < 30; i++) {
-            fixed.add(generateSentence(3, 3));
-            variable.add(generateSentence(10, 20));
-        }
-        CustomRecyclerViewAdapter customRecyclerViewAdapter = new CustomRecyclerViewAdapter(MainActivity.this, fixed, variable);
-        rv.setAdapter(customRecyclerViewAdapter);
+        one.add("Hello, how is work?");
+        two.add("Hello, everything is fine, thank you for asking.");
+        one.add("?");
+        two.add("How are you today?");
+        one.add("I not feeling well!");
+        two.add("Are you sick, you seem extremely tired?");
+        one.add("I've got flu");
+        two.add("Go home and please stay there until you feel better. " +
+                "You don't want to spread your infection!");
+
+        rv_before = (RecyclerView) findViewById(R.id.rv_before);
+        rv_before.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManagerB = new LinearLayoutManager(MainActivity.this);
+        rv_before.setLayoutManager(linearLayoutManagerB);
+
+        rv_after = (RecyclerView) findViewById(R.id.rv_after);
+        rv_after.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManagerA = new LinearLayoutManager(MainActivity.this);
+        rv_after.setLayoutManager(linearLayoutManagerA);
+
+        CustomRecyclerViewAdapter customRecyclerViewAdapterB = new CustomRecyclerViewAdapter(MainActivity.this, true, one, two);
+        rv_before.setAdapter(customRecyclerViewAdapterB);
+
+        CustomRecyclerViewAdapter customRecyclerViewAdapterA = new CustomRecyclerViewAdapter(MainActivity.this, false, one, two);
+        rv_after.setAdapter(customRecyclerViewAdapterA);
 
         b = (Button) findViewById(R.id.b);
         b.setOnClickListener(new View.OnClickListener() {
@@ -48,30 +64,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 List<String> fixed = new ArrayList<>();
                 List<String> variable = new ArrayList<>();
-                for (int i = 0; i < 30; i++) {
-                    fixed.add(generateSentence(3, 3));
-                    variable.add(generateSentence(10, 20));
+                for (int i = 0; i < one.size(); i++) {
+                    int oneI = new Random().nextInt(one.size());
+                    fixed.add(one.get(oneI));
+                    variable.add(two.get(oneI));
                 }
-                customRecyclerViewAdapter.changeText(fixed, variable);
+                customRecyclerViewAdapterB.changeText(fixed, variable);
+                customRecyclerViewAdapterA.changeText(fixed, variable);
             }
         });
-    }
-
-    private String generateSentence(int maxWordSize, int maxSentenceWordCount) {
-        char[] chars = new char[]{'a', 'b', 'c', 'd', ' ', 'e', 'f', 'g', 'h'};
-        StringBuilder sentence = new StringBuilder();
-        int word_size = new Random().nextInt(maxWordSize) + 1;
-        int sentence_word_count = new Random().nextInt(maxSentenceWordCount) + 2;
-        for (int i = 0; i < sentence_word_count; i++) {
-            StringBuilder word = new StringBuilder();
-            for (int j = 0; j < word_size; j++) {
-                char c = chars[new Random().nextInt(chars.length)];
-                word.append(c);
-            }
-            sentence.append(word);
-            sentence.append(" ");
-        }
-        return sentence.toString();
     }
 
     private class CustomRecyclerViewAdapter extends RecyclerView.Adapter<CustomRecyclerViewAdapter.CustomViewHolder> {
@@ -79,10 +80,13 @@ public class MainActivity extends AppCompatActivity {
         private final Context context;
         private List<String> fixed;
         private List<String> variable;
+        private final boolean before;
 
-        public CustomRecyclerViewAdapter(Context context, List<String> fixed, List<String> variable) {
+        public CustomRecyclerViewAdapter(Context context, boolean before,
+                                         List<String> fixed, List<String> variable) {
             super();
             this.context = context;
+            this.before = before;
             this.fixed = fixed;
             this.variable = variable;
         }
@@ -96,7 +100,14 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
-            holder.textViewInADifferentWay.setText(fixed.get(position), variable.get(position));
+            if(!before) { // after
+                holder.textViewInADifferentWay.setText(fixed.get(position), variable.get(position));
+            }else{ // before
+                TextView tv_fixed = (TextView) holder.text_view_in_a_different_way.findViewById(R.id.tv_fixed);
+                TextView tv_variable = (TextView) holder.text_view_in_a_different_way.findViewById(R.id.tv_variable);
+                tv_fixed.setText(fixed.get(position));
+                tv_variable.setText(variable.get(position));
+            }
             holder.tv.setText("Item " + position + " top");
             holder.tv1.setText("Item " + position + " bottom");
         }
@@ -108,16 +119,19 @@ public class MainActivity extends AppCompatActivity {
 
         public class CustomViewHolder extends RecyclerView.ViewHolder {
 
-            private final TextViewInADifferentWay textViewInADifferentWay;
+            private TextViewInADifferentWay textViewInADifferentWay;
+            private final View text_view_in_a_different_way;
             private final TextView tv;
             private final TextView tv1;
 
             public CustomViewHolder(@NonNull View itemView) {
                 super(itemView);
-                View text_view_in_a_different_way = (View)
+                text_view_in_a_different_way = (View)
                         itemView.findViewById(R.id.text_view_in_a_different_way);
-                textViewInADifferentWay = new TextViewInADifferentWay(context,
-                        text_view_in_a_different_way);
+                if(!before) { // after
+                    textViewInADifferentWay = new TextViewInADifferentWay(context,
+                            text_view_in_a_different_way);
+                }
                 tv = (TextView) itemView.findViewById(R.id.tv);
                 tv1 = (TextView) itemView.findViewById(R.id.tv1);
             }
